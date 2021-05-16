8d10190306b13185c1d3f0d03ae58f53f4827ee7
Rodrigo Vasconcelos de Barros <4694457-rodrigopk@users.noreply.gitlab.com>
2021-05-16 15:37:16 +0200

    Add fallback for image header

--- src/app/prizes/presentation/components/image_header.tsx
+++ src/app/prizes/presentation/components/image_header.tsx
@@ -8,7 +8,11 @@ import nobelHeroTablet from '../assets/nobel_hero_tablet.jpeg';
 
 export const ImageHeader: React.FC<{}> = () => (
   <>
-    <Image src={nobelHeroTablet} alt="nobel_hero_tablet" />
+    <Image
+      src={nobelHeroTablet}
+      alt="nobel_hero_tablet"
+      fallbackSrc="https://via.placeholder.com/2560x960?text=nobel_hero_tablet.jpeg"
+    />
     <Text variant="caption" color="gray.600" mt={1} ml={2}>
       © Nobel Media. Photo: Nanaka Adachi
     </Text>
